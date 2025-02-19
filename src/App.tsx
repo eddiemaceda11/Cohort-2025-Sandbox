ae2009412eddd83b6e108b74790c9cc9f6ef1f03
Eddie Maceda <eddiemaceda11@gmail.com>
2025-02-18 22:49:43 -0700

    Create the guest management section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* GUEST MANAGEMENT */}
+      <h1>Guest Management</h1>
+      <div>
+        <div>
+          <h2>Upcoming visits</h2>
+          <div>
+            <div className="guest-info">
+              <p>John Doe</p>
+              <p>104838</p>
+              <span>Pending</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
